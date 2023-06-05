042603420a74e45268fa77e2e6532be888d7f83c
Leonardo Costa <devleonardo27@gmail.com>
2023-06-05 14:18:08 -0300

    Aula 26 de entrada de dados segunda parte

---
+++ src/entrada_de_dados/Aula25.java
@@ -0,0 +1,18 @@
+package entrada_de_dados;
+
+import java.util.Scanner;
+
+public class Aula25 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String x;
+
+        x = sc.next();
+        System.out.println("Valor do x: " + x);
+
+        sc.close();
+    }
+}
---
+++ src/entrada_de_dados/Aula26Parte2EntradaDeDados.java
@@ -0,0 +1,22 @@
+package entrada_de_dados;
+
+import java.util.Scanner;
+
+public class Aula26Parte2EntradaDeDados {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s1, s2, s3;
+        s1 = sc.nextLine();
+        s2 = sc.nextLine();
+        s3 = sc.nextLine();
+
+        System.out.println("Dados digitados:");
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+
+        sc.close();
+    }
+}
