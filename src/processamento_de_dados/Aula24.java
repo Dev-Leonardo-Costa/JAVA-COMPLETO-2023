4c7490c42eb816d198445c0a4fc9c7b358e382dc
Leonardo Costa <devleonardo27@gmail.com>
2023-06-05 13:48:36 -0300

    Aula de processamento de dados usando Casting e comando de atribuiçao

---
+++ src/processamento_de_dados/Aula24.java
@@ -0,0 +1,28 @@
+package processamento_de_dados;
+
+public class Aula24 {
+
+    public static void main(String[] args) {
+        int x;
+        double y;
+
+        x = 5;
+        y = 2 * x;
+
+        System.out.println(x);
+        System.out.println(y);
+
+        double area, a = 6.0, b = 8.0, c = 5.0;
+
+        area = (a + b) / 2 * c;
+
+        System.out.println(area);
+
+        int f = 5, u = 2;
+        double result;
+
+        result = (double) f / u;
+
+        System.out.println(result);
+    }
+}
