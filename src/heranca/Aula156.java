0168b21314105da3a1f63aca202631e3dcd26c9a
Leonardo Costa <devleonardo27@gmail.com>
2023-06-14 14:00:31 -0300

    Aula 156 herança

---
+++ src/heranca/Aula156.java
@@ -0,0 +1,7 @@
+package heranca;
+
+public class Aula156 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/heranca/entities/ContaNegocio.java
@@ -0,0 +1,33 @@
+package heranca.entities;
+
+public class ContaNegocio extends Conta {
+
+    private double limiteEmprestimo;
+
+    public ContaNegocio() {
+        super();
+    }
+
+    public ContaNegocio(double limiteEmprestimo) {
+        this.limiteEmprestimo = limiteEmprestimo;
+    }
+
+    public ContaNegocio(Integer numero, String titular, Double balance, double limiteEmprestimo) {
+        super(numero, titular, balance);
+        this.limiteEmprestimo = limiteEmprestimo;
+    }
+
+    public double getLimiteEmprestimo() {
+        return limiteEmprestimo;
+    }
+
+    public void setLimiteEmprestimo(double limiteEmprestimo) {
+        this.limiteEmprestimo = limiteEmprestimo;
+    }
+
+    public void emprestimo(double valorEmprestimo) {
+        if (limiteEmprestimo <= valorEmprestimo) {
+            saldo += valorEmprestimo - 10.0;
+        }
+    }
+}
