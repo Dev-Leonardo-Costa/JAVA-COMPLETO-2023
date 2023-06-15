5f7a65632ee871b41a25772101bcade3ac19b8ac
Leonardo Costa <devleonardo27@gmail.com>
2023-06-15 11:42:53 -0300

    Aula 176 solução boa

---
+++ src/excecao/excecoes_personalizada_solucao_03_boa/model/Exception/DomainException.java
@@ -0,0 +1,9 @@
+package excecao.excecoes_personalizada_solucao_03_boa.model.Exception;
+
+public class DomainException extends Exception{
+
+    public DomainException(String message) {
+        super(message);
+    }
+
+}
