7497a8a000998ebe569f42be6a802791b96ceeb8
Leonardo Costa <devleonardo27@gmail.com>
2023-06-16 23:48:51 -0300

    Aula 278 Criando a clase Saller

--- src/demo_dao_jdbc/application/Program.java
+++ src/demo_dao_jdbc/application/Program.java
@@ -1,12 +1,16 @@
 package demo_dao_jdbc.application;
 
 import demo_dao_jdbc.model.entities.Department;
+import demo_dao_jdbc.model.entities.Saller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
 
         Department department = new Department(1, "Livro");
-        System.out.println(department);
+        Saller saller = new Saller(1, "Leo", "leo@gmail.com", new Date(), 3000.0, department);
+        System.out.println(saller);
 
     }
 }
