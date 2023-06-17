ba4dc9f0332a585ebd6c833833f081c7e0014b10
Leonardo Costa <devleonardo27@gmail.com>
2023-06-16 21:22:03 -0300

    Aula 270 abrindo conexão para buscar os dados da base de dados e fechando a conexão

--- src/projeto_com_jdbc/db/Program.java
+++ src/projeto_com_jdbc/db/Program.java
@@ -1,12 +1,45 @@
 package projeto_com_jdbc.db;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Program {
     public static void main(String[] args) {
 
-        Connection connection = DB.getConnection();
-        DB.closeConnect();
+        Connection connection = null;
+
+        Statement st = null;
+        ResultSet rs = null;
+
+        try {
+
+            connection = DB.getConnection();
+            st = connection.createStatement();
+
+            rs = st.executeQuery("Select * from department");
+
+            while (rs.next()) {
+                StringBuilder sb = new StringBuilder()
+                        .append("Id: ")
+                        .append(rs.getInt("id")).append(", ")
+                        .append("Name: ")
+                        .append(rs.getString("name"));
+                sb.toString();
+
+                System.out.println(sb);
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+
+            DB.closeResultSet(rs);
+            DB.closeStatement(st);
+            DB.closeConnect();
+
+        }
 
     }
 }
