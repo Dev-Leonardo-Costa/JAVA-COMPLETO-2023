d99e473d28de2a6fbbbe61038669289c13ce3e8b
Leonardo Costa <devleonardo27@gmail.com>
2023-06-17 01:54:11 -0300

    Seller tirando so mais L

--- src/demo_dao_jdbc/model/factory/DaoFactory.java
+++ src/demo_dao_jdbc/model/factory/DaoFactory.java
@@ -1,13 +1,13 @@
 package demo_dao_jdbc.model.factory;
 
 import demo_dao_jdbc.model.dao.SellerDao;
-import demo_dao_jdbc.model.impl.SelllerDaoJDBC;
+import demo_dao_jdbc.model.impl.SellerDaoJDBC;
 import projeto_com_jdbc.db.DB;
 
 public class DaoFactory {
 
     public static SellerDao createSellerDao(){
-        return new SelllerDaoJDBC(DB.getConnection());
+        return new SellerDaoJDBC(DB.getConnection());
     }
 
 }
