53e7d73c13485c0f865aeb9816e354fa07458f42
Leonardo Costa <devleonardo27@gmail.com>
2023-06-23 10:32:14 -0300

    Entidade User e Resources

---
+++ src/course/src/main/java/com/educaweb/course/controlles/UserController.java
@@ -0,0 +1,18 @@
+package com.educaweb.course.controlles;
+
+import com.educaweb.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User user = new User(1L, "Leonardo", "leo@gmail.com","992370872","123456");
+        return ResponseEntity.ok().body(user);
+    }
+}
